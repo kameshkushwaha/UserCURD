0e673d2b6a5b25e50b12e2ca34a1b64014f69438
Kamesh Kushwaha <kameshkushwaha2001@gmail.com>
2024-03-02 18:06:29 +0530

    addng the method for deleting the data from database By ID

--- UserCURD/src/main/java/com/curd/Controlar/MainContolar.java
+++ UserCURD/src/main/java/com/curd/Controlar/MainContolar.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,6 +69,24 @@ public class MainContolar {
 			return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
 		}
 	}
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<?> deleteUser(@PathVariable Integer id) {
+		Map<String, Object> map = new LinkedHashMap<String, Object>();
+		try {
+			User user = userService.findById(id);
+			userService.delete(user);
+			map.put("status", 1);
+			map.put("message", "Record is deleted successfully!");
+			return new ResponseEntity<>(map, HttpStatus.OK);
+		} catch (Exception ex) {
+			map.clear();
+			map.put("status", 0);
+			map.put("message", "Data is not found");
+			return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+		}
+	}
+
 
 
     
